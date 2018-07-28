34ff0a05d371fe431ac2827b12abd3f6892c7cb8
NEET <zxc455233@gmail.com>
2018-07-29 01:11:36 +0800

    strategy main.py

--- strategy/script/main.py
+++ strategy/script/main.py
@@ -36,10 +36,11 @@ def callback(data):
     ## Orbit
     if abs(data.blue_ang) > ATTACK_RANGE_ANG:
       oV, oW = oF.fuzzy(dis, ang)
-      orbit_speed = 20
+      orbit_speed = 20 * np.sign(data.blue_ang) * -1
       twist.linear.x  = dis*math.sin(math.radians(ang+180))/abs(dis)*oV + orbit_speed
       twist.linear.y  = dis*math.cos(math.radians(ang))/abs(dis)*oV
       twist.angular.z = oW
+    ## Attack
     else:
       twist.linear.x  = 0
       twist.linear.y  = 0
